af268d1d3f830df568b52230f0380818f66b20c0
jovanamika <jmika939@gmail.com>
2022-03-15 19:23:18 +0100

    Update GeneticAlgorithm.java
    dodana funkcija za ukrštanje

--- src/genetski/GeneticAlgorithm.java
+++ src/genetski/GeneticAlgorithm.java
@@ -8,34 +8,46 @@ public class GeneticAlgorithm {
 	public Random rand = new Random();
 	private Population population = new Population();
 	double fitness, secondFitness;
-	int count = 0;
+	public static int count = 0;
 	public static int vertex_num = 5;
 
 	public class Population {
 		int size = 2;
 		Individual[] individuals = new Individual[2];
+		Individual[] children = new Individual[2]; //niz za djecu;
+		
 
 		public Population() {
 			individuals[0] = new Individual();
 			individuals[0].initialize();
 			individuals[1] = new Individual();
 			individuals[1].initializeSecondI(individuals[0]); // --> inicijalizujemo dvije pocetne jedinke
+			for(int i = 0; i<2; i++) 
+				children[i] = new Individual();
 		}
 
-		//Funckija za ispis gena (pocetni nizovi 0-1 )
+		// Funckija za ispis gena (pocetni nizovi 0-1 )
 		public void writeIndividualsGenes() {
 			for (int i = 0; i < 2; i++) {
-				System.out.println(i + ". niz: ");
+				System.out.println(i + ". roditelj: ");
 				for (int j = 0; j < individuals[i].genes.length; j++) {
 					System.out.print(individuals[i].genes[j] + " ");
+				}
+				System.out.println();
+			}
+			
+			for (int i = 0; i < 2; i++) {
+				System.out.println(i + ". dijete: ");
+				for (int j = 0; j < children[i].genes.length; j++) {
+					System.out.print(children[i].genes[j] + " ");
 				}
 				System.out.println();
 			}
 		}
 
 	}
-    
-	//klasa za jedinke
+
+	// klasa za jedinke
 	public class Individual {
 		public int fitness;
 		public int[] genes = new int[vertex_num];
@@ -63,8 +75,46 @@ public class GeneticAlgorithm {
 		}
 	}
 
+	public void selection() {
+       
+	}
+
+	public void crossover() {
+		int n = rand.nextInt(vertex_num);
+		System.out.println(n);
+		//first child
+        for(int i = 0; i<n; i++) {
+			population.children[0].genes[i] = population.individuals[0].genes[i]; 
+        }
+        for (int i = n; i<vertex_num; i++) {
+        	population.children[0].genes[i] = population.individuals[1].genes[i]; 
+        }
+        
+        //second child
+        for(int i = 0; i<n; i++) {
+			population.children[1].genes[i] = population.individuals[1].genes[i]; 
+        }
+        for (int i = n; i<vertex_num; i++) {
+        	population.children[1].genes[i] = population.individuals[0].genes[i]; 
+        }
+   
+        System.out.println("N: "+n);
+        population.writeIndividualsGenes(); // --> ispis roditelja i djece poslije ukrstanja ( samo za testiranje)
+        
+        
+	}
+
 	public static void main(String[] args) {
 		GeneticAlgorithm ga = new GeneticAlgorithm();
-		ga.population.writeIndividualsGenes();
+		// ga.population.writeIndividualsGenes();
+
+		// ga.population.setFitness()
+		while (count < 10) {
+			// ga.selection();
+			count = 10;
+			ga.crossover();
+
+			// ga.mutation();
+		}
 	}
 }
